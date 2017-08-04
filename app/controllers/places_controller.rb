63ce2c0188860fa4525c1c1a338b8a958980a806
mdunnettstone <mdunnettstone@gmail.com>
2017-08-04 15:32:16 +0000

    Only user that created place can edit or delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -20,16 +24,33 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if current_user != @place.user
+      return render text: 'You must have created the place to edit it!', status: :forbidden
+    end
   end
 
   def update
-    @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
-    redirect_to place_path
+  @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render text: 'Not Allowed', status: :forbidden
+  end
+
+  @place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if current_user != @place.user
+      return render text: 'You must have created the place to delete it!', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
